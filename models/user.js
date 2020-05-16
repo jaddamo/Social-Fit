4926706f5040a79b5c489bdb86ea777eb99b8141
jelly <gouldljane@gmail.com>
2020-05-16 13:00:40 -0400

    signup post route

--- models/user.js
+++ models/user.js
@@ -22,9 +22,19 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       unique: true
     }
-
   });
-  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+
+  //Association 
+  User.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Favorite, {
+      onDelete: "cascade",
+      allowNull: false
+    });
+  };
+
+// Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
